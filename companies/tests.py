f007f7cdddc8120b886b1f38aabff3170cbb5b2d
josewails <joseritch45@gmail.com>
2021-03-27 13:40:06 +0300

    add a simple test case

--- companies/tests.py
+++ companies/tests.py
@@ -1,3 +1,34 @@
+import json
+
 from django.test import TestCase
 
-# Create your tests here.
+from faker import Faker
+
+fake = Faker()
+
+
+class TestCompanies(TestCase):
+
+    def test_company_create(self):
+        # test a non-valid ares won't work
+
+        url = "/"
+
+        data = dict(
+            name=fake.name(),
+            email=fake.email(),
+            ico="fadfksdf"
+        )
+
+        response = self.client.post(url, data=data)
+        json_response = json.loads(response.content.decode('utf-8'))
+
+        self.assertEqual(json_response["success"], False)
+
+        # test a valid ares will work
+
+        data.update(dict(ico="68407700"))
+
+        response = self.client.post(url, data)
+        json_response = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(json_response["success"], True)
